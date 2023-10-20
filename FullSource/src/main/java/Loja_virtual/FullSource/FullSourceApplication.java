42ef79a2e3b55f4d7c16f762c893b579a9410af3
deividferraz <deividferraz20@gmail.com>
2023-10-20 01:36:37 -0300

    primeiro commit

---
+++ FullSource/src/main/java/Loja_virtual/FullSource/FullSourceApplication.java
@@ -0,0 +1,13 @@
+package Loja_virtual.FullSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullSourceApplication.class, args);
+	}
+
+}
---
+++ FullSource/src/test/java/Loja_virtual/FullSource/FullSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package Loja_virtual.FullSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
